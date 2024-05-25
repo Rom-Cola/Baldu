208ea75e82759ae401c299d773987bc313528be3
Loiev Roman <robolito0420@gmail.com>
2024-05-26 02:07:36 +0300

    Fixed profile page

--- BalduApp/models.py
+++ BalduApp/models.py
@@ -9,17 +9,19 @@ class User(AbstractUser):
     interests = models.TextField(blank=True)
     marital_status = models.CharField(max_length=50, choices=(('', ''), ('S', 'Самотній/Самотня'), ('M', 'Одружений/Заміжня'), ('D', 'Розлучений/Розлучена'), ('W', 'Вдівець/Вдова')), blank=True)
     orientation = models.CharField(max_length=50, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
 
     groups = models.ManyToManyField(
         'auth.Group',
-        related_name='custom_user_set',  # Додано related_name
+        related_name='custom_user_set',
         blank=True,
         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
         related_query_name='user',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
-        related_name='custom_user_set',  # Додано related_name
+        related_name='custom_user_set',
         blank=True,
         help_text='Specific permissions for this user.',
         related_query_name='user',
