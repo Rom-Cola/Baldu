c8cdf8c7cfc1572a41204ea06445ff6d901198d2
SashaKolodko <sashanight69@gmail.com>
2024-05-25 04:22:07 +0300

    Restoring the login rewriting logins and registration

--- BalduApp/forms.py
+++ BalduApp/forms.py
@@ -1,9 +1,14 @@
 # BalduApp/forms.py
 
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
+class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'age', 'gender', 'interests')
+        fields = ['username', 'password1', 'password2']
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
